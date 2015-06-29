997b289b54af428fe79fb660575ba19ea8ddbff6
Javi Manzano <javi.manzano.oller@gmail.com>
2015-06-29 21:48:27 +0100

    Enemies move in zigzag

---
+++ Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMover : MonoBehaviour {
+	
+	public float speed;
+	public Boundary boundary;
+
+	private int direction;
+	
+	void Start () {
+		GetComponent<Rigidbody>().velocity = transform.forward * speed;
+
+		// Get a random direction - 0 or 1
+		direction = Random.Range(0, 2);
+	}
+	
+	void FixedUpdate () {
+		// 0 or 1 translated to left and right
+		float x;
+		if (direction == 0) {
+			x = -1.0f;
+		} else {
+			x = 1.0f;
+		}
+
+		Vector3 movement = new Vector3 (x, 0.0f, transform.forward.z);
+		GetComponent<Rigidbody>().velocity = movement * speed;
+		
+		GetComponent<Rigidbody>().position = new Vector3 
+		(
+			Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+		);
+
+		// If it hits a boundary, change direction
+		if (direction == 1 && GetComponent<Rigidbody> ().position.x >= boundary.xMax) {
+			direction = 0;
+		} else if (direction == 0 && GetComponent<Rigidbody> ().position.x <= boundary.xMin) {
+			direction = 1;
+		}
+	}
+}
--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,9 +2,9 @@
 using System.Collections;
 
 public class Mover : MonoBehaviour {
-
+	
 	public float speed;
-
+	
 	void Start () {
 		GetComponent<Rigidbody>().velocity = transform.forward * speed;
 	}
