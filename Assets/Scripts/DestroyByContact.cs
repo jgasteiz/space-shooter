58a63ce20e1c3c707f84da8e5cbf8f5f26d6f4be
Javi Manzano <javi.manzano.oller@gmail.com>
2015-07-23 21:51:08 +0100

    Add new destroy by contact logic

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,14 +4,15 @@ using System.Collections.Generic;
 
 public class DestroyByContact : MonoBehaviour
 {
-
+	
 	public GameObject explosion;
 	public GameObject playerExplosion;
 	public int scoreValue;
-
+	
 	private GameController gameController;
 	private List<string> tagsToIgnore;
-
+	private List<string> indestructibleObjectTags;
+	
 	void Start ()
 	{
 		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
@@ -21,31 +22,35 @@ public class DestroyByContact : MonoBehaviour
 		if (gameController == null) {
 			Debug.Log ("Cannot find 'GameController' script");
 		}
-
+		
 		tagsToIgnore = new List<string>() {"Boundary", "EnemyBolt", "EnemyShip"};
+		
+		indestructibleObjectTags = new List<string>() {"Bomb"};
 	}
-
+	
 	void OnTriggerEnter (Collider other)
-	{
+	{	
 		// Ignore collissions with ignored tags.
 		if (tagsToIgnore.Contains(other.tag)) {
 			return;
 		}
-
+		
 		// Explosion!
 		Instantiate (explosion, transform.position, transform.rotation);
-
+		
 		// Player explosion
 		if (other.tag == "Player") {
 			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
 			gameController.PlayerDies ();
 		}
-
+		
 		gameController.AddScore (scoreValue);
 
 		// Destroy the game object - bolt or player
-		Destroy (other.gameObject);
-
+		if (indestructibleObjectTags.Contains (other.tag) == false) {
+			Destroy (other.gameObject);
+		}
+		
 		// Destroy the game object itself
 		Destroy (gameObject);
 	}
---
+++ Assets/Scripts/DestroyByContactIndestructible.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DestroyByContactIndestructible : MonoBehaviour
+{
+	private List<string> tagsToDestroy;
+	
+	void Start ()
+	{
+		tagsToDestroy = new List<string>() {"EnemyBolt", "EnemyShip"};
+	}
+	
+	void OnTriggerEnter (Collider other)
+	{
+		
+		// Destroy the game object - bolt or player
+		if (tagsToDestroy.Contains (other.tag)) {
+			Destroy (other.gameObject);
+		}
+	}
+}
